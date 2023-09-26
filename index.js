72d6f47fec84c6b4fe59d36ca4c991bea335bd0b
Shadrack kipkorir <shadybett540@gmail.com>
2023-09-26 12:28:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = {...employee};
+    updatedEmployee[key]= value;
+    return updatedEmployee;
+}
+const employee = {
+    name:"Sam",
+    streetAddress:"Broadway",
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+const newEmployee = {
+    name:"Sam",
+    streetAddress:"12 Broadway"
+};
+function deleteFromEmployeeByKey(employee,keyToDelete){
+    const employeeClone = {...employee};
+    if(keyToDelete in employeeClone){
+        delete employeeClone[keyToDelete];
+    }
+    return employeeClone;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,keyToDelete) {
+    delete employee[keyToDelete];
+    return employee;
+}
